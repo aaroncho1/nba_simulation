212a3d61449e2a36245d2d026600dc146c10508f
Aaron <aaroncho10@gmail.com>
2022-02-28 23:19:46 -0500

    edited teams class

--- game.rb
+++ game.rb
@@ -6,18 +6,23 @@ class NbaSimulationGame
     attr_accessor :game_clock
     
     #1 nba quarter has 720 second quarters * 4 = 2880
-    def initialize
-        @teams = []
+    def initialize(away_team, home_team)
+        @away_team, @home_team = away_team, home_team
         @players = []
         @game_clock = 2880
+        @current_team = @away_team
     end
 
     def game_over?
         game_clock <= 0
     end
 
+    def play_possession
+    end
+
     def run
         until game_over?
+            play_possession
         end
     end
 
