5b0653a14fa554e63eb0d4cc4efb097ffcb2bddb
Aaron <aaroncho10@gmail.com>
2022-02-28 23:04:35 -0500

    added run method in game

--- game.rb
+++ game.rb
@@ -1,3 +1,16 @@
 class NbaSimulationGame
+    require_relative 'display'
+    require_relative 'teams'
+    require_relative 'player'
+
+    def initialize
+        @teams = []
+        @players = []
+    end
+
+    def run
+        until game_over?
+        end
+    end
 
 end
