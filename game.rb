11d6a1ae759d6a941c894101e374ee82f6070a98
Aaron <aaroncho10@gmail.com>
2022-02-28 23:14:29 -0500

    added game_over? method in game

--- game.rb
+++ game.rb
@@ -3,9 +3,17 @@ class NbaSimulationGame
     require_relative 'teams'
     require_relative 'player'
 
+    attr_accessor :game_clock
+    
+    #1 nba quarter has 720 second quarters * 4 = 2880
     def initialize
         @teams = []
         @players = []
+        @game_clock = 2880
+    end
+
+    def game_over?
+        game_clock <= 0
     end
 
     def run
