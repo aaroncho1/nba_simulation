2da7aec44b04e3267587659bb3e80bc6ee9e00a2
Aaron <aaroncho10@gmail.com>
2022-03-04 00:46:25 -0500

    added switch team method in game class

--- game.rb
+++ game.rb
@@ -1,13 +1,12 @@
 require_relative 'display'
 require_relative 'teams'
-require 'byebug'
+# require 'byebug'
 
 class NbaSimulationGame
 
     attr_accessor :game_clock
     attr_reader :display
 
-    
     #1 nba quarter has 720 second quarters * 4 = 2880
     def initialize(away_team, home_team)
         @away_team, @home_team = away_team, home_team
@@ -78,8 +77,12 @@ class NbaSimulationGame
         @offensive_team
     end
 
+    def switch_team
+        @offensive_team = @offensive_team == @away_team ? @home_team : @away_team
+    end
+
     def run
-        debugger
+        # debugger
         tip_off_result
         until first_quarter_over?
             play_possession
@@ -106,7 +109,7 @@ end
 # suns and warriors offensive result frequencies in an array
 #2m/a = 2pt fg made/ missed , 3m/a = 3pt made/ missed sf/nsf = shooting/ non shooting foul on other team, or = offensive rebound, to = turnover
 suns_frequencies = (["2m"] * 32) + (["2a"] * 30) + (["3m"] * 11) + (["3a"] * 20) + (["sf"] * 7) + (["nsf"] * 2) + (["or"] * 10) + (["to"] * 9)
-warriors_frequencies = (["2m"] * 26) + (["2a"] * 21) + (["3m"] * 14) + (["3a"] * 25) + (["f"] * 11) + (["nsf"] * 2) (["or"] * 10) + (["to"] * 13)
+warriors_frequencies = (["2m"] * 26) + (["2a"] * 21) + (["3m"] * 14) + (["3a"] * 25) + (["f"] * 11) + (["nsf"] * 2) + (["or"] * 10) + (["to"] * 13)
 suns_ft_frequencies = (["ftm"] * 16) + (["fta"] * 4)
 warriors_ft_frequencies = (["ftm"] * 16) + (["fta"] * 6)
 NbaSimulationGame.new(Team.new("Phoenix Suns", 0, suns_frequencies, suns_ft_frequencies), Team.new("Golden State Warriors", 0, warriors_frequencies, warriors_ft_frequencies)).run
--- teams.rb
+++ teams.rb
@@ -1,16 +1,17 @@
 class Team 
-    attr_reader :name, :posession_frequencies, :ft_frequencies
-    attr_accessor :score, :team_fouls
+    attr_reader :name
+    attr_accessor :score, :team_fouls, :possession_frequencies, :ft_frequencies
     def initialize(name, score, possession_frequencies, ft_frequencies)
         @name = name
         @score = score
-        @posession_frequencies, @ft_frequencies = posession_frequencies, ft_frequencies
+        @possession_frequencies = possession_frequencies
+        @ft_frequencies = ft_frequencies
         @team_fouls = 0
     end
 
     def get_result
-        rand_ind = rand(posession_frequencies.length)
-        result = posession_frequencies.shuffle[rand_ind]
+        rand_ind = rand(possession_frequencies.length)
+        result = possession_frequencies.shuffle[rand_ind]
         result
     end
 
