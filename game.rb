8fb20f733f19bdf55d0324454faca2bfeb99dee0
Aaron <aaroncho10@gmail.com>
2022-03-04 02:01:15 -0500

    added overtime method in game class

--- game.rb
+++ game.rb
@@ -3,14 +3,14 @@ require_relative 'teams'
 # require 'byebug'
 
 class NbaSimulationGame
-
-    attr_accessor :game_clock
     attr_reader :display
+    attr_accessor :game_clock, :overtime_clock
 
     #1 nba quarter has 720 second quarters * 4 = 2880
     def initialize(away_team, home_team)
         @away_team, @home_team = away_team, home_team
         @game_clock = 2880
+        @overtime_clock = 300
         @offensive_team = nil
         @defensive_team = nil
         @display = Display.new(0)
@@ -22,10 +22,6 @@ class NbaSimulationGame
 
     def foul
         "f"
-    end
-
-    def game_over?
-        game_clock <= 0
     end
 
     def score_team(made_shot)
@@ -51,9 +47,9 @@ class NbaSimulationGame
     def simulate_game_clock
         rand_time = rand(100)
         if rand_time.between?(0,12)
-            game_clock -= fast_break_time
+            @game_clock -= fast_break_time
         else    
-            game_clock -= possession_time
+            @game_clock -= possession_time
         end
     end
 
@@ -91,6 +87,10 @@ class NbaSimulationGame
         game_clock <= 720
     end
 
+    def game_over?
+        game_clock <= 0
+    end
+
     def tip_off_result
         tip_off_index = rand(2)
         @offensive_team = tip_off_index == 0 ? @away_team : @home_team
@@ -103,7 +103,30 @@ class NbaSimulationGame
         @offensive_team = @offensive_team == @away_team ? @home_team : @away_team
     end
 
-    def final_score
+    def tie?
+        @away_team.score == @home_team.score
+    end
+
+
+    def go_to_overtime
+        display.possession_results << "----------OVERTIME----------"
+        tip_off_result
+        until game_over?
+            play_possession
+            switch_team
+        end
+        go_to_overtime if tie?
+    end
+
+    def end_of_regulation
+        display.possession_results << "------END OF REGULATION-----"
+        if tie?
+            go_to_overtime
+        else
+            puts "#{@away_team.name} : #{@away_team.score}"
+            puts "#{@home_team.name} : #{@home_team.score}"
+        end
+    end
 
     def run
         # debugger
@@ -127,7 +150,8 @@ class NbaSimulationGame
             play_possession
             switch_team
         end
-        final_score
+        end_of_regulation
+        play_by_play_results
     end
 end
 
