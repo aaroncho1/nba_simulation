964c89204adf438950aaac14ab04af3556cb82c5
Aaron <aaroncho10@gmail.com>
2022-03-12 02:38:51 -0500

    added points and possessions in player class

--- players.rb
+++ players.rb
@@ -1,9 +1,12 @@
 class Player
 
     attr_reader :name, :position, :minutes, :frequencies
+    attr_accessor :points, :posessions
 
     def initialize(name, position, minutes, frequencies)
-        @name, @position, @minutes, @frequencies        
+        @name, @position, @minutes, @frequencies   
+        @points = 0
+        @posessions = []     
     end
 
 end
