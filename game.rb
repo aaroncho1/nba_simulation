e05956b84d9775fe2faedfd480a40037f550e52f
Aaron <aaroncho10@gmail.com>
2022-03-21 20:19:24 -0400

    added ft tendencies for each player and tested byebug to make sure no errors

--- game.rb
+++ game.rb
@@ -249,25 +249,25 @@ end
 #2m/a = 2pt fg made/ missed , 3m/a = 3pt made/ missed sf/nsf = shooting/ non shooting foul on other team, or = offensive rebound, to = turnover
 suns_frequencies = (["2m"] * 32) + (["2a"] * 30) + (["3m"] * 11) + (["3a"] * 20) + (["sf"] * 7) + (["nsf"] * 2) + (["to"] * 9)
 suns_players = [
-        Player.new("C. Paul", "PG", 37, (["2a"] * 9) + (["3a"] * 3) + (["sf"] * 2) + (["dr"] * 5) + (["or"] * 1)),
-        Player.new("D. Booker", "SG", 38, (["2a"] * 14) + (["3a"] * 7) + (["sf"] * 3) + (["dr"] * 5) + (["or"] * 1)),
-        Player.new("M. Bridges", "SF", 38, (["2a"] * 6) + (["3a"] * 4) + (["sf"] * 1) + (["dr"] * 4) + (["or"] * 1)),
-        Player.new("C. Johnson", "PF", 30, (["2a"] * 3) + (["3a"] * 6) + (["sf"] * 1) + (["dr"] * 4) + (["or"] * 1)),
-        Player.new("D. Ayton", "C", 32, (["2a"] * 12) + (["3a"] * 0) + (["sf"] * 1) + (["dr"] * 10) + (["or"] * 3)),
-        Player.new("J. Crowder", "SF", 25, (["2a"] * 3) + (["3a"] * 5) + (["sf"] * 1) + (["dr"] * 5) + (["or"] * 1)),
-        Player.new("C. Payne", "PG", 20, (["2a"] * 6) + (["3a"] * 4) + (["sf"] * 1) + (["dr"] * 3) + (["or"] * 1)),
-        Player.new("F. Kaminsky", "PF", 20, (["2a"] * 5) + (["3a"] * 2) + (["sf"] * 1) + (["dr"] * 4) + (["or"] * 1))
+        Player.new("C. Paul", "PG", 37, (["2a"] * 9) + (["3a"] * 3) + (["sf"] * 2) + (["dr"] * 5) + (["or"] * 1), (["make"] * 8) + (["miss"] * 2)),
+        Player.new("D. Booker", "SG", 38, (["2a"] * 14) + (["3a"] * 7) + (["sf"] * 3) + (["dr"] * 5) + (["or"] * 1), (["make"] * 9) + (["miss"] * 1)),
+        Player.new("M. Bridges", "SF", 38, (["2a"] * 6) + (["3a"] * 4) + (["sf"] * 1) + (["dr"] * 4) + (["or"] * 1), (["make"] * 8) + (["miss"] * 2)),
+        Player.new("C. Johnson", "PF", 30, (["2a"] * 3) + (["3a"] * 6) + (["sf"] * 1) + (["dr"] * 4) + (["or"] * 1), (["make"] * 9) + (["miss"] * 1)),
+        Player.new("D. Ayton", "C", 32, (["2a"] * 12) + (["3a"] * 0) + (["sf"] * 1) + (["dr"] * 10) + (["or"] * 3), (["make"] * 7) + (["miss"] * 3)),
+        Player.new("J. Crowder", "SF", 25, (["2a"] * 3) + (["3a"] * 5) + (["sf"] * 1) + (["dr"] * 5) + (["or"] * 1), (["make"] * 8) + (["miss"] * 2)),
+        Player.new("C. Payne", "PG", 20, (["2a"] * 6) + (["3a"] * 4) + (["sf"] * 1) + (["dr"] * 3) + (["or"] * 1), (["make"] * 8) + (["miss"] * 2)),
+        Player.new("F. Kaminsky", "PF", 20, (["2a"] * 5) + (["3a"] * 2) + (["sf"] * 1) + (["dr"] * 4) + (["or"] * 1), (["make"] * 9) + (["miss"] * 1))
 ]
 warriors_frequencies = (["2m"] * 26) + (["2a"] * 21) + (["3m"] * 14) + (["3a"] * 25) + (["sf"] * 11) + (["nsf"] * 2) + (["to"] * 13)
 warriors_players = [
-        Player.new("S. Curry", "PG", 37, (["2a"] * 8) + (["3a"] * 12) + (["sf"] * 3) + (["dr"] * 5) + (["or"] * 1)),
-        Player.new("K. Thompson", "SG", 35, (["2a"] * 8) + (["3a"] * 8) + (["sf"] * 1) + (["dr"] * 4) + (["or"] * 1)),
-        Player.new("A. Wiggins", "SF", 35, (["2a"] * 9) + (["3a"] * 5) + (["sf"] * 2) + (["dr"] * 4) + (["or"] * 1)),
-        Player.new("D. Green", "PF", 37, (["2a"] * 5) + (["3a"] * 2) + (["sf"] * 1) + (["dr"] * 7) + (["or"] * 1)),
-        Player.new("J. Wiseman", "C", 26, (["2a"] * 9) + (["3a"] * 1) + (["sf"] * 1) + (["dr"] * 6) + (["or"] * 1)),
-        Player.new("J. Poole", "PG", 34, (["2a"] * 6) + (["3a"] * 7) + (["sf"] * 2) + (["dr"] * 3) + (["or"] * 1)),
-        Player.new("J. Kuminga", "PF", 16, (["2a"] * 4) + (["3a"] * 2) + (["sf"] * 1) + (["dr"] * 3) + (["or"] * 1)),
-        Player.new("K. Looney", "C", 20, (["2a"] * 5) + (["3a"] * 0) + (["sf"] * 1) + (["dr"] * 8) + (["or"] * 2))
+        Player.new("S. Curry", "PG", 37, (["2a"] * 8) + (["3a"] * 12) + (["sf"] * 3) + (["dr"] * 5) + (["or"] * 1), (["make"] * 9) + (["miss"] * 1)),
+        Player.new("K. Thompson", "SG", 35, (["2a"] * 8) + (["3a"] * 8) + (["sf"] * 1) + (["dr"] * 4) + (["or"] * 1), (["make"] * 9) + (["miss"] * 1)),
+        Player.new("A. Wiggins", "SF", 35, (["2a"] * 9) + (["3a"] * 5) + (["sf"] * 2) + (["dr"] * 4) + (["or"] * 1), (["make"] * 6) + (["miss"] * 4)),
+        Player.new("D. Green", "PF", 37, (["2a"] * 5) + (["3a"] * 2) + (["sf"] * 1) + (["dr"] * 7) + (["or"] * 1), (["make"] * 6) + (["miss"] * 4)),
+        Player.new("J. Wiseman", "C", 26, (["2a"] * 9) + (["3a"] * 1) + (["sf"] * 1) + (["dr"] * 6) + (["or"] * 1), (["make"] * 6) + (["miss"] * 4)),
+        Player.new("J. Poole", "PG", 34, (["2a"] * 6) + (["3a"] * 7) + (["sf"] * 2) + (["dr"] * 3) + (["or"] * 1), (["make"] * 9) + (["miss"] * 1)),
+        Player.new("J. Kuminga", "PF", 16, (["2a"] * 4) + (["3a"] * 2) + (["sf"] * 1) + (["dr"] * 3) + (["or"] * 1), (["make"] * 7) + (["miss"] * 3)),
+        Player.new("K. Looney", "C", 20, (["2a"] * 5) + (["3a"] * 0) + (["sf"] * 1) + (["dr"] * 8) + (["or"] * 2), (["make"] * 6) + (["miss"] * 4))
 ]
 suns_ft_frequencies = (["ftm"] * 16) + (["fta"] * 4)
 warriors_ft_frequencies = (["ftm"] * 16) + (["fta"] * 6)
--- players.rb
+++ players.rb
@@ -3,11 +3,13 @@ class Player
     attr_reader :name, :position, :minutes, :frequencies
     attr_accessor :points, :rebounds, :posessions
 
-    def initialize(name, position, minutes, frequencies)
+    def initialize(name, position, minutes, frequencies, ft_per)
         @name, @position = name, position  
         @minutes, @frequencies = minutes, frequencies 
+        @ft_per = ft_per
         @points = 0
         @rebounds = 0
+        @assists = 0
         @posessions = []     
     end
 
